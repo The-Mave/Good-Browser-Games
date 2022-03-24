14afc17e74c768082cb509b37597fe2d6b539307
rodrigo <rodrigo.scavassin@gmail.com>
2022-03-24 19:49:58 -0300

    criando PUT de admin/categorias

--- Routes/web/adminRoutes.js
+++ Routes/web/adminRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
+import bodyParser from "body-parser"
 import adminController from "../../controllers/web/adminController.js"
 
 const router = express.Router();
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+router.use(urlencodedParser);
 
 router
     .get("/admin/dashboard", adminController.dashboard)
@@ -11,7 +14,7 @@ router
     .get("/admin/usuarios", adminController.adminObterUsuarios)
 
     .get("/admin/jogos/edit/:id", adminController.adminEditarJogos)
-    .get("/admin/categorias/edit/:id", adminController.adminEditarCategorias)
+    .get("/admin/categorias/edit/:id", urlencodedParser, adminController.adminEditarCategorias)
     .get("/admin/usuarios/edit/:id", adminController.adminEditarUsuarios)
 
 
