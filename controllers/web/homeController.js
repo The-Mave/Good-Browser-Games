875f8fb9fbe0c97e448155e0ff24a8fcb76ab5fb
matheus.avelino@levelup.com.br <matheus.avelino@levelup.com.br>
2022-05-29 01:40:38 -0300

    JWT AUTH

--- controllers/web/homeController.js
+++ controllers/web/homeController.js
@@ -4,23 +4,12 @@ const index = (req,res) => {
     res.render("Home/index");
 }
 
-const login = (req,res) => {
-    console.log("Render de index funcionou!")
-    res.render("Home/login");
-}
-
-const signup = (req,res) => {
-    console.log("Render de index funcionou!")
-    res.render("Home/signup");
-}
 const perfil =(req,res) =>{
     console.log("Você entrou no perfil")
     res.render("Home/perfil");
 }
 export default {
     index,
-    login,
-    signup,
     perfil
 
   };
